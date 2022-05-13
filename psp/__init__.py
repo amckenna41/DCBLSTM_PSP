c06765d7eea6104c312f865e5f6c90f697264649
40175607 <amckenna41@qub.ac.uk>
2022-05-14 00:19:40 +0100

    updating init files

--- psp/__init__.py
+++
@@ -1,7 +0,0 @@
-
-__author__ = 'Adam McKenna, https://github.com/amckenna41'
-__authorEmail__ = 'amckenna41@qub.ac.uk'
-__url__ = 'https://github.com/amckenna41/DCBLSTM_PSP'
-__credits__ = ['Adam McKenna']
-__version__ = "1.0.0"
-__license__ = 'MIT'
--- psp_gcp/__init__.py
+++ psp_gcp/__init__.py
@@ -1,7 +1,8 @@
 
-__author__ = 'Adam McKenna, https://github.com/amckenna41'
+__author__ = 'AJ McKenna, https://github.com/amckenna41'
 __authorEmail__ = 'amckenna41@qub.ac.uk'
 __url__ = 'https://github.com/amckenna41/DCBLSTM_PSP'
-__credits__ = ['Adam McKenna']
-__version__ = "1.0.0"
+__credits__ = ['AJ McKenna']
+__version__ = "1.0.2"
 __license__ = 'MIT'
+__name__ = 'DCBLSTM_PSP'
